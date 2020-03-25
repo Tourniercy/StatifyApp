fe0ea7224d20461323399b1a6d8b30e96aabf371
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2020-03-25 14:30:18 +0100

    pause/play

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\AuthController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use SpotifyWebAPI;
 
 class AjaxController extends AbstractController
 {
@@ -16,7 +20,40 @@ class AjaxController extends AbstractController
             'controller_name' => 'AjaxController',
         ]);
     }
-    public function pause() {
-        return $this->json(['username' => 'jane.doe']);
+    /**
+     * @Route("/pause", name="pause")
+     */
+    public function pause(Request $request, SessionInterface $session )
+    {
+        $accessToken = $session->get('accessToken');
+        if( ! $accessToken ) {
+            $session->getFlashBag()->add('error', 'Invalid authorization');
+            $this->redirectToRoute('login');
+        }
+        $api = new SpotifyWebAPI\SpotifyWebAPI();
+        $api->setAccessToken($accessToken);
+
+        $session->set('current_track', $api->getMyCurrentTrack()); // symfony session
+        $api->pause();
+        return $this->json(['url' => $session->get('current_track')->item->uri,'progress_ms' => $session->get('current_track')->progress_ms]);
+    }
+    /**
+     * @Route("/play", name="play")
+     */
+    public function play(Request $request, SessionInterface $session )
+    {
+        $accessToken = $session->get('accessToken');
+        if( ! $accessToken ) {
+            $session->getFlashBag()->add('error', 'Invalid authorization');
+            $this->redirectToRoute('login');
+        }
+        $api = new SpotifyWebAPI\SpotifyWebAPI();
+        $api->setAccessToken($accessToken);
+        $api->play(false, [
+            'uris' => [ $session->get('current_track')->item->uri],
+            'offset' => [1000],
+        ]
+        );
+        return($api->getMyCurrentPlaybackInfo());
     }
 }
