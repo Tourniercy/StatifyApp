2dc332b0e9e43f744203547cab80f315b419e0d2
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2020-05-09 13:45:46 +0200

    url

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -30,7 +30,6 @@ class AuthController extends AbstractController
                 'playlist-modify-private','user-follow-read','user-follow-modify','user-library-read','user-top-read','user-read-playback-state','streaming' ]
         ];
         $url = $_SERVER['APP_ENV'] === 'prod' ? 'https://statifyapp.herokuapp.com/login/oauth' :  'http://127.0.0.1:8000/login/oauth';
-        dd($url);
         $this->spotify = new SpotifyWebAPI\Session(
             $this->spotifyParams['client_id'],
             $this->spotifyParams['client_secret'],
